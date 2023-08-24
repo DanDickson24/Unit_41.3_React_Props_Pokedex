a4ea8fa15d1955c7f43a2ea8d7a8067bdf00ec1a
Dan Dickson <dandickson24@gmail.com>
2023-08-24 17:46:16 -0400

    new

---
+++ src/Pokecard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Pokecard.css';
+
+function Pokecard({ id, name, type, baseExperience }) {
+  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+  return (
+    <div className="pokecard">
+      <h3>{name}</h3>
+      <img src={imageUrl} alt={name} />
+      <p>Type: {type}</p>
+      <p>Base Experience: {baseExperience}</p>
+    </div>
+  );
+}
+
+export default Pokecard;
